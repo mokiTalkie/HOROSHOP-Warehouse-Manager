5ebc0be45b0718b21a36d6e50cafa2f94e93e8d5
mokiTalkie <moki.main@gmail.com>
2024-09-23 16:44:52 +0300

    Added method for DB init to check if all required collections are present

--- database/config.py
+++ database/config.py
@@ -12,4 +12,16 @@ DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING").replace(
     "<db_password>", DB_PASSWORD
 )
 
-DB_CLIENT = MongoClient(DB_CONNECTION_STRING)
+DB_CLIENT = MongoClient(DB_CONNECTION_STRING)[f"{os.getenv("DB_NAME")}"]
+
+def initialize_database() -> None:
+    required_collections = ["Shelves", "Racks", "Reserved", "Service", "Delivered"]
+    existing_collections = DB_CLIENT.list_collections()
+
+    for collection in required_collections:
+        if collection not in existing_collections:
+            DB_CLIENT.create_collection(name=collection)
+            
+            print("Collection created")
+
+initialize_database()
