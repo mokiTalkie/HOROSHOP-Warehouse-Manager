6530838d11b07fa83d85ada2bf2ff351218e9b23
mokiTalkie <moki.main@gmail.com>
2024-09-23 16:45:47 +0300

    Added some functions to make operations with DB

---
+++ database/operations.py
@@ -0,0 +1,26 @@
+from .config import DB_CLIENT
+
+
+def add_product():
+    pass
+
+def replace_product():
+    pass
+
+def reserve_product():
+    pass
+
+def delete_product():
+    pass
+
+def add_shelf():
+    pass
+
+def delete_shelf():
+    pass
+
+def add_rack():
+    pass
+
+def delete_rack():
+    pass
