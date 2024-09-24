3cd40b50d3641dadba28aa6fa523a76239b5822b
mokiTalkie <moki.main@gmail.com>
2024-09-24 13:32:51 +0300

    Added params for DB functions

--- backend/database/operations.py
+++ backend/database/operations.py
@@ -1,26 +1,27 @@
 from .config import DB_CLIENT
+from .models import Product
 
 
-def add_product():
+def add_product(product: Product, is_new: bool, rack_id: int, shelf_id: int) -> bool:
     pass
 
-def replace_product():
+def replace_product(product: Product, from_rack_id: int, from_shelf_id: int, to_rack_id: int, to_shelf_id: int) -> bool:
     pass
 
-def reserve_product():
+def reserve_product(products: dict[str | int]) -> bool:
     pass
 
-def delete_product():
+def delete_product(product: Product) -> bool:
     pass
 
-def add_shelf():
+def add_shelf(rack_id: int) -> bool:
     pass
 
-def delete_shelf():
+def delete_shelf(shelf_id: int, rack_id: int) -> bool:
     pass
 
-def add_rack():
+def add_rack() -> bool:
     pass
 
-def delete_rack():
+def delete_rack(rack_id: int) -> bool:
     pass
