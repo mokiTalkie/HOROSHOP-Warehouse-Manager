55b6d452bfce0c3af2c4015c8aef30ff18a277d8
mokiTalkie <moki.main@gmail.com>
2024-10-04 16:03:27 +0300

    Models updated. Added Supplier, Category models

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,21 +1,38 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr, HttpUrl
+
+class Contacts(BaseModel):
+    email: EmailStr | None
+    phone: str = Field(pattern=r"^\+380\d{9}$")
+    url: HttpUrl | None
 
 class User(BaseModel):
     id: int = Field(alias="_id")
+    email: EmailStr
+    name: str
+
+class Supplier(BaseModel):
+    id: int = Field(alias="_id")
+    name: str
+
+class Category(BaseModel):
+    id: int = Field(alias="_id")
+    parent_id: int
     name: str
 
 class Product(BaseModel):
     id: str
+    category_id: int
+    article: str
     name: str
 
-class Order(BaseModel):
-    id: int
+# class Order(BaseModel):
+#     id: int
 
-class Shelf(BaseModel):
-    id: int
+# class Shelf(BaseModel):
+#     id: int
 
-class Rack(BaseModel):
-    id: int
+# class Rack(BaseModel):
+#     id: int
 
-class Warehouse(BaseModel):
-    name: str
+# class Warehouse(BaseModel):
+#     name: str
