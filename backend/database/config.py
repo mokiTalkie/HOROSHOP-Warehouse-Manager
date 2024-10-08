ff34b8deb4a68b0089507c596fa2bfa09d1251ce
Владимир Касьянов <moki.main@gmail.com>
2024-10-08 09:03:04 +0300

    Initialize DB func dropped

--- backend/database/config.py
+++ backend/database/config.py
@@ -1,10 +1,9 @@
 import os
-from pymongo.mongo_client import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
@@ -12,16 +11,4 @@ DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING").replace(
     "<db_password>", DB_PASSWORD
 )
 
-DB_CLIENT = MongoClient(DB_CONNECTION_STRING)[f"{os.getenv("DB_NAME")}"]
-
-def initialize_database() -> None:
-    required_collections = ["Shelves", "Racks", "Reserved", "Service", "Delivered"]
-    existing_collections = DB_CLIENT.list_collections()
-
-    for collection in required_collections:
-        if collection not in existing_collections:
-            DB_CLIENT.create_collection(name=collection)
-            
-            print("Collection created")
-
-initialize_database()
+DB_CLIENT = AsyncIOMotorClient(DB_CONNECTION_STRING)[f"{os.getenv("DB_NAME")}"]
