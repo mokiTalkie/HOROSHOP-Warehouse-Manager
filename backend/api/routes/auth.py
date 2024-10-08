ab91a811c6b8fbbf53547f78273770d91ada6248
Владимир Касьянов <moki.main@gmail.com>
2024-10-08 09:03:54 +0300

    Auth route updated

--- backend/api/routes/auth.py
+++ backend/api/routes/auth.py
@@ -1,52 +1,61 @@
 import os
+
+from authlib.integrations.starlette_client import OAuth, OAuthError
+from fastapi import APIRouter, Request
 from starlette.config import Config
-from starlette.responses import RedirectResponse, HTMLResponse
-from authlib.integrations.starlette_client import OAuth
-from authlib.integrations.starlette_client import OAuthError
-from fastapi import Request, APIRouter
+from starlette.responses import HTMLResponse, RedirectResponse
+
+from backend.utils.security import validate_user
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 # OAuth settings
-GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID') or None
-GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET') or None
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID") or None
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET") or None
 if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
-    raise BaseException('Missing env variables')
+    raise BaseException("Missing env variables")
 
 # Set up oauth
-config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
+config_data = {
+    "GOOGLE_CLIENT_ID": GOOGLE_CLIENT_ID,
+    "GOOGLE_CLIENT_SECRET": GOOGLE_CLIENT_SECRET,
+}
 starlette_config = Config(environ=config_data)
 oauth = OAuth(starlette_config)
 oauth.register(
-    name='google',
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_kwargs={'scope': 'openid email profile'},
+    name="google",
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_kwargs={"scope": "openid email profile"},
 )
 
 router = APIRouter()
 
+
 @router.get("/")
 async def login_page():
-    return HTMLResponse('<a href=/auth/login>Login</a>')
+    return HTMLResponse("<a href=/auth/login>Login</a>")
+
 
 @router.get("/login")
 async def login(request: Request):
-    redirect_uri = request.url_for('auth')
+    redirect_uri = request.url_for("auth")
     return await oauth.google.authorize_redirect(request, redirect_uri)
+
 
 @router.route("/auth")
 async def auth(request: Request):
     try:
         access_token = await oauth.google.authorize_access_token(request)
     except OAuthError:
-        return RedirectResponse(url="/")
+        return RedirectResponse(url="/auth")
     
-    print(access_token)
-    user_email = access_token.get("userinfo")["email"]
-    print(user_email)
-    # request.session['user'] = dict(user_email)
+    user_info = access_token.get("userinfo")
+    if await validate_user(user_info=user_info):
+        print("User found!")
 
+
+
+    request.session["user"] = 1
     return RedirectResponse(url="/")
