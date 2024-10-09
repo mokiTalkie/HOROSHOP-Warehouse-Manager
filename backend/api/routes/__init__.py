21d0033b9a03eb226e4dd19c4f4e0030de2dc209
mokiTalkie <moki.main@gmail.com>
2024-10-09 17:32:27 +0300

    Router for general routes added

---
+++ backend/api/routes/__init__.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Request
+from starlette.responses import HTMLResponse
+from ...utils.security import auth_required
+
+
+router = APIRouter()
+
+@router.get("/")
+@auth_required
+async def index(request: Request):
+    return {"ALL": "WORKS"}
+
+@router.get("/me")
+@auth_required
+async def me(request: Request):
+    return request.session['user_data']
