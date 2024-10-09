3d2049400e1e66069dea9ac1c83ae6d4dea46fc5
mokiTalkie <moki.main@gmail.com>
2024-10-09 17:32:48 +0300

    Auth wrapper updated

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -6,23 +6,22 @@ from backend.database.models import User
 from backend.database.operations import get_user, update_user
 
 def auth_required(func):
-    @wraps
-    async def wrapper(*args, **kwargs):
-        request: Request = kwargs.get("request")
+    @wraps(func)
+    async def wrapper(request: Request, *args, **kwargs):
         token = request.session.get("access_token")
         token_expire_date = request.session.get("token_expire_date")
 
         if not token or time.time() > token_expire_date:
             return RedirectResponse("/auth")
         
-        return await func(*args, **kwargs)
+        return await func(request, *args, **kwargs)
     return wrapper
 
 async def validate_user(user_info: dict[str: str]) -> User | None:
     if user_info["email_verified"]:
         user_from_db = await get_user(email=user_info["email"])
         if user_from_db:
-            print("User found!")
             update_user(user=User(_id=user_from_db.id, email=user_from_db.email, name=user_info["given_name"], photo=user_info["picture"]))
+            return user_from_db
     else:
         return None
